8fda70ca956030e2f0b60026785532b9d9b3a981
peterahlstrom <ahlstrom.peter@gmail.com>
2015-12-28 09:57:09 +0100

    Initial commit

---
+++ train_info.js
@@ -0,0 +1,124 @@
+var http = require('http');
+var request = require('request');
+var fs = require('fs');
+var cachedRequest = require('cached-request')(request);
+var cacheDirectory = 'cache/';
+var server = require('./lib/server');
+var url = 'http://api.trafikinfo.trafikverket.se/v1/data.json';
+var xmlReq = 'req_template.xml';
+var config = JSON.parse(fs.readFileSync('config.json').toString())
+var trainId;
+var getStationNames = require('./lib/getStationNames');
+var stationNames;
+var timeUpdated = new Date();
+cachedRequest.setCacheDirectory(cacheDirectory);
+
+function readReq(trainId){
+	return  fs.readFileSync(xmlReq).toString()
+				.replace('{trainId}', trainId)
+				.replace('{apikey}', config.api_key);
+}
+
+function post(trainId, callback, res){
+	var xmlReq = readReq(trainId.toString());
+	cachedRequest(
+	    {
+	    method: 'POST',
+	    url:url,
+	    body : xmlReq,
+	    headers: {'Content-Type': 'text/xml'},
+	    ttl: 1200
+	    },
+	    function (error, response, body) {
+	    	if (error) {
+	    		console.error(error);
+	    		return;
+	    	}
+	        if (!error && response.statusCode == 200) {
+	            timeUpdated = new Date();
+	            callback(body, res);
+	        }
+	    }
+	);
+}
+
+function prepareData(data, res) {
+	// merge data into new object
+	data = JSON.parse(data);
+	var stops = [],
+		tempStops = [],
+		l = {};
+	if (!data.RESPONSE.RESULT[0].TrainAnnouncement) {
+		res.end('No train with this number');
+		console.log('No train with this number');
+		return;
+	}
+	var data = data.RESPONSE.RESULT[0].TrainAnnouncement;
+
+	data.forEach(function(loc){
+		if (tempStops.indexOf(loc.LocationSignature) === -1){
+			l = {};
+			l[loc.LocationSignature] = {'Ankomst': {}, 'Avgang': {}};
+			tempStops.push(loc.LocationSignature);
+			stops.push(l);
+		}
+
+		try {
+		l[loc.LocationSignature][loc.ActivityType] = loc;
+		}
+		catch(err) {
+			 console.error(err);
+			return;
+		}
+	});
+	displayData(stops, res);
+}
+
+function displayData(stops, res) {
+	// create html and write response
+	var output = {};
+	output['stops'] = [];
+
+	function parseTime(timeString) {
+		if (timeString){
+			return timeString.split('T').slice(-1).toString().substring(0,5);
+		}
+	}
+	stops.forEach(function(s){
+		var loc = Object.keys(s)[0]
+		var p = '<p>';
+		var stop = s[loc];
+		var stop_data = {
+			location: stationNames[loc],
+			ank_time: stop.Ankomst.AdvertisedTimeAtLocation ? parseTime(stop.Ankomst.AdvertisedTimeAtLocation) : false,
+			avg_time: stop.Avgang.AdvertisedTimeAtLocation ? parseTime(stop.Avgang.AdvertisedTimeAtLocation) : false,
+			track: stop.Avgang.TrackAtLocation ? stop.Avgang.TrackAtLocation : false,
+			deviation: stop.Deviation ? stop.Deviation : false,
+			real_ank: stop.Ankomst.TimeAtLocation ? parseTime(stop.Ankomst.TimeAtLocation) : false,
+			ber_avg: stop.Avgang.EstimatedTimeAtLocation ? parseTime(stop.Avgang.EstimatedTimeAtLocation) : false
+		};
+		output.stops.push(stop_data)
+	});
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost')
+	res.writeHead(200, {'Content-type': 'text/plain'});
+	res.end(JSON.stringify(output));
+
+}
+
+function onRequest(req,res){
+	if (isNaN(req.url.slice(1))) {
+		res.end('Invalid train number.');
+		console.log('Invalid train number');
+		return;
+	}
+	trainId = req.url.slice(1);
+	console.log('Request for train nr ' + trainId)
+	post(trainId, prepareData, res);
+}
+
+function init(stnNames) {
+	stationNames = JSON.parse(stnNames);
+	server(3000, onRequest);
+}
+
+getStationNames(init);
